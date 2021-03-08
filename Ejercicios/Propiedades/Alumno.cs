273cc8eefbb1af1785213b63d917949444c31159
Ingrid Trochez <ingridtrochez99@gmail.com>
2021-03-08 12:10:45 -0600

    Agregando programa Propiedades y constructores

---
+++ Ejercicios/Propiedades/Alumno.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class Alumno
+{
+    public int Id { get; set; }
+
+    public String PrimerNombre { get; set; }
+
+    public String SegundoNombre { get; set; }
+
+    public Alumno(){
+        Id = 1;
+    }
+    
+    public Alumno(int id){
+        Id = id;
+    }
+}
---
+++ Ejercicios/Propiedades/propiedades/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace propiedades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Alumno a = new Alumno();
+            a.PrimerNombre = "Juan";
+            a.SegundoNombre = "Pedro";
+
+            Alumno b = new Alumno();
+            b.Id = 2;
+            b.PrimerNombre = "Maria";
+            b.SegundoNombre = "Martha";
+
+            Alumno c = new Alumno(3);
+            c.PrimerNombre = "Pablo";
+
+            Console.WriteLine(a.Id);
+            Console.WriteLine(b.Id);
+            Console.WriteLine(c.Id);
+
+        }
+    }
+}
